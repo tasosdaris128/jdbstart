0fc14404842914969bc778f5bf6d6cbdf3315b9e
Tasos Daris <tasos.daris@datawise.ai>
2023-10-09 20:00:00 +0300

    feat: Playing along with the connection pool.
    Refs: #
    Committed-by: Tasos Daris<t.daris@datawise.com>
    Signed-off-by: Tasos Daris <tasos.daris@datawise.ai>

--- src/main/java/com/tasos/jdbstart/App.java
+++ src/main/java/com/tasos/jdbstart/App.java
@@ -1,10 +1,43 @@
 package com.tasos.jdbstart;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.tasos.jdbstart.db.MainConnectionPool;
 import com.tasos.jdbstart.logger.Log;
 
 public class App {
     public static void main(String args[]) {
         Log.init(true);
-        Log.flex();
+        
+        // @Cleanup: Remove flex call.
+        // Log.flex();
+
+        try {
+            // @Todo: Retrieve from environment variable.
+            MainConnectionPool connectionPool = MainConnectionPool.create("jdbc:postgresql://localhost/jdbstart", "postgres", "postgres");
+
+            Connection aConnection = connectionPool.getConnection();
+            Connection bConnection = connectionPool.getConnection();
+
+            Log.i("Initial size of connection pool: %d", connectionPool.size());
+            Log.i("Initial count of used connections: %d", connectionPool.countUsed());
+
+            boolean releaseResultA = connectionPool.releaseConnection(aConnection);
+            
+            Log.i("Result of releasing connection A: %b", releaseResultA);
+            Log.i("Size of connection pool after releasing: %d", connectionPool.size());
+            Log.i("Count of used connections after release: %d", connectionPool.countUsed());
+
+            boolean releaseResultB = connectionPool.releaseConnection(bConnection);
+
+            Log.i("Result of releasing connection B: %b", releaseResultB);
+            Log.i("Size of connection pool after releasing: %d", connectionPool.size());
+            Log.i("Count of used connections after release: %d", connectionPool.countUsed());
+
+            connectionPool.shutdown();
+        } catch (SQLException e) {
+            Log.exc(e);
+        }
     }
 }
