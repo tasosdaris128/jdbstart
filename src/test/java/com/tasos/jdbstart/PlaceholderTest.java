17a68c5a2e79a24a0685b73de714a4288562e9e9
Tasos Daris <tasos.daris@datawise.ai>
2023-10-10 18:02:50 +0300

    test: Adding a simple demo jUnit test.
    Refs: #
    Committed-by: Tasos Daris<t.daris@datawise.com>
    Signed-off-by: Tasos Daris <tasos.daris@datawise.ai>

---
+++ src/test/java/com/tasos/jdbstart/PlaceholderTest.java
@@ -0,0 +1,14 @@
+package com.tasos.jdbstart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlaceholderTest {
+
+    @Test
+    public void placeholder() {
+        assertEquals(1, 1);
+    }
+    
+}
