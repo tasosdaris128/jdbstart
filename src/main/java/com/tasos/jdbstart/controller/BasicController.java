0c539a2825fcd98734f3015846aba9a40c641e12
Tasos Daris <tasos.daris@datawise.ai>
2023-10-10 11:25:27 +0300

    feat: Converting the BasicController into a parent controller class (with a method for responding and a method for parsing the incoming request body).
    Refs: #
    Committed-by: Tasos Daris<t.daris@datawise.com>
    Signed-off-by: Tasos Daris <tasos.daris@datawise.ai>

--- src/main/java/com/tasos/jdbstart/controller/BasicController.java
+++ src/main/java/com/tasos/jdbstart/controller/BasicController.java
@@ -6,6 +6,16 @@ import com.tasos.jdbstart.logger.Log;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+
 import com.sun.net.httpserver.HttpExchange;
 
 public class BasicController implements HttpHandler {
@@ -20,10 +30,28 @@ public class BasicController implements HttpHandler {
         
         String message = "{\"message\":\"OK\"}";
         
+        respond(httpExchange, code, message);
+    }
+
+    public void respond(HttpExchange httpExchange, int code, String message) throws IOException {
         httpExchange.sendResponseHeaders(code, message.length());
         OutputStream outputStream = httpExchange.getResponseBody();
         outputStream.write(message.getBytes());
         outputStream.close();
+    }
+
+    public <T> T parse(InputStream in, Class<T> valueType) throws IOException, JsonMappingException {
+        String requestBody = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8))
+            .lines()
+            .collect(Collectors.joining("\n"));
+        
+        Log.i("Request body: %s", requestBody);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        T o = mapper.readValue(requestBody, valueType);
+
+        return o;
     }
 
 }
